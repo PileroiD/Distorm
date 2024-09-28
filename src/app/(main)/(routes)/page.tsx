e442412cb79ec3a2b47ae2c751660f709f1ecfec
Alex-HP <aleksandr.lozinskiy2005@gmail.com>
2024-09-28 19:19:01 -0400

    folders setup

---
+++ src/app/(main)/(routes)/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+    return (
+        <div>
+            <Button className="m-10">Some button</Button>
+        </div>
+    );
+}
