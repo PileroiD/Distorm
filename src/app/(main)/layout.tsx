307e5b26c0e307c91a97e8b0ac0da4eb122ecb88
Alex-HP <aleksandr.lozinskiy2005@gmail.com>
2024-10-03 23:46:14 -0400

    create server modal

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -7,9 +7,10 @@ interface MainlayoutProps {
 async function MainLayout({ children }: MainlayoutProps) {
     return (
         <div className="h-full">
-            <div className="hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
+            <div className="sm:hidden md:flex h-full w-[72px] z-30 flex-col fixed inset-y-0">
                 <NavigationSidebar />
             </div>
+
             <main className="md:pl-[72px] h-full">{children}</main>
         </div>
     );
