ee71d1f1230adea49f64275df6902906197418e0
Alex-HP <aleksandr.lozinskiy2005@gmail.com>
2024-10-23 22:57:16 -0400

    uploadthing callback url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,9 @@ import { ThemeProvider } from "@/components/providers/ThemeProvider";
 import { ModalProvider } from "@/components/providers/ModalProvider";
 import SocketProvider from "@/components/providers/SocketProvider";
 import QueryProvider from "@/components/providers/QueryProvider";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+import { ourFileRouter } from "./api/uploadthing/core";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -34,7 +37,14 @@ export default function RootLayout({
                     >
                         <SocketProvider>
                             <ModalProvider />
-                            <QueryProvider>{children}</QueryProvider>
+                            <QueryProvider>
+                                <NextSSRPlugin
+                                    routerConfig={extractRouterConfig(
+                                        ourFileRouter
+                                    )}
+                                />
+                                {children}
+                            </QueryProvider>
                         </SocketProvider>
                     </ThemeProvider>
                 </body>
